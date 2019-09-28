2344c7b05b892edec3cbbbf867a3c2d8a8492052
zgbjgg <zgbjgg@gmail.com>
2019-09-28 12:37:53 -0500

    add base lib

---
+++ lib/quetzal.ex
@@ -0,0 +1,18 @@
+defmodule Quetzal do
+  @moduledoc """
+  Documentation for Quetzal.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Quetzal.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
