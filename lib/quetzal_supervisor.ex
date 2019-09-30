f29aab80939ae80d7efe7a4cb14b6bbdb8e90cba
zgbjgg <zgbjgg@gmail.com>
2019-09-30 17:44:55 -0500

    add supervisor to use for registry

---
+++ lib/quetzal_supervisor.ex
@@ -0,0 +1,27 @@
+defmodule Quetzal.Supervisor do
+  use Supervisor
+
+  def start_link(opts) do
+    name =
+      opts[:name] ||
+        raise ArgumentError, "the :name option is required when starting Quetzal Registry"
+
+    sup_name = Module.concat(name, "Supervisor")
+    Supervisor.start_link(__MODULE__, opts, name: sup_name)
+  end
+
+  def init(opts) do
+    name = opts[:name]
+
+    registry = [
+      keys: :duplicate,
+      name: name
+    ]
+
+    children = [
+      {Registry, registry},
+    ]
+
+    Supervisor.init(children, strategy: :rest_for_one)
+  end
+end
