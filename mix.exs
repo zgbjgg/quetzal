5678fbf8b90cbd4a761389a403c573ae3bc487ee
zgbjgg <zgbjgg@gmail.com>
2019-10-07 18:48:51 -0500

    bump version 0.1.4

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Quetzal.MixProject do
   def project do
     [
       app: :quetzal,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       package: package(),
