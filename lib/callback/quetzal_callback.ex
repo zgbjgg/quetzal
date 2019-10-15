e7549750ae4da99b823c2b6fa5377392e5cc1868
zgbjgg <zgbjgg@gmail.com>
2019-10-14 20:05:13 -0500

    fix process to fire event from callback since target could be empty

--- lib/callback/quetzal_callback.ex
+++ lib/callback/quetzal_callback.ex
@@ -13,39 +13,40 @@ defmodule Quetzal.Callback do
   end
 
   @impl true
-  def handle_call({:dispatch, _opts, _event, []}, _from, state), do: {:reply, [], state}
   def handle_call({:dispatch, opts, event, params}, _from, state) do
     # get target changed and get its value from params
-    [target|_] = params
-    |> Map.get("_target")
+    with [target|_] <- Map.get(params, "_target")
+    do
+      # since targets could be wrapped in a form, send all changes
+      # in order to allow clients to use cascade updates
+      values = params
+      |> Map.delete("_target")
 
-    # since targets could be wrapped in a form, send all changes
-    # in order to allow clients to use cascade updates
-    values = params
-    |> Map.delete("_target")
-    |> Map.values
+      # build arguments
+      args = [event, target, values]
 
-    # build arguments
-    args = [event, target, values]
+      # get module and functions to call
+      mod = opts[:handler]
+      funs = opts[:callbacks]
 
-    # get module and functions to call
-    mod = opts[:handler]
-    funs = opts[:callbacks]
+      output = funs
+      |> Enum.map(fn fun ->
+           try do
+             :erlang.apply(mod, fun, args)
+           rescue
+             error ->
+               Logger.error "Quetzal.Callback Error trying to apply:\n" <>
+                 "#{inspect mod}:#{inspect fun}(#{inspect args})\n" <>
+                 "caused by: #{inspect error}"
+               {:error, error}
+           end
+      end)
+      |> :lists.append
 
-    output = funs
-    |> Enum.map(fn fun ->
-         try do
-           :erlang.apply(mod, fun, args)
-         rescue
-           error ->
-             Logger.error "Quetzal.Callback Error trying to apply:\n" <>
-               "#{inspect mod}:#{inspect fun}(#{inspect args})\n" <>
-               "caused by: #{inspect error}"
-             {:error, error}
-         end
-    end)
-    |> :lists.append
-
-    {:reply, output, state}
+      {:reply, output, state}
+    else
+      [] ->
+        {:reply, [], state}
+    end
   end
 end
